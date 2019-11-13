ba45eda4b721ef43693e18a790cd442c7f16beb7
andrews-felipe <ferulip@gmail.com>
2019-11-13 18:34:57 -0300

    Criação de questionário de curso

--- kahoot/src/app/curso/curso.component.ts
+++ kahoot/src/app/curso/curso.component.ts
@@ -9,12 +9,20 @@ import { MenuService } from '../components/menu/menu.service';
 export class CursoComponent implements OnInit {
 
   readonly title = "Cursos"
+
+  question_name : string
   course : Object
+  view_flag : boolean
+  iterator : number = 1
   
   content = [
     { status : true, title : 'Teorias da Linguagem', qntd : 10, createdAt : '13 Out 2019'},
     { status : true, title : 'Estrutura de dados', qntd : 10, createdAt : '13 Out 2019'},
     { status : true, title : 'Revisão de Orientação Objetos', qntd : 10, createdAt : '13 Out 2019'}
+  ]
+
+  questions = [
+    
   ]
 
   constructor(private route: ActivatedRoute, private service : MenuService) { }
@@ -24,6 +32,13 @@ export class CursoComponent implements OnInit {
 
     this.route.queryParams.subscribe(params => {this.course = params});
     
+  }
+
+  add(){
+    this.questions.push({id : this.iterator, name : this.question_name})
+    this.iterator++;
+    this.question_name = ""
+    console.log(this.questions)
   }
 
 }
