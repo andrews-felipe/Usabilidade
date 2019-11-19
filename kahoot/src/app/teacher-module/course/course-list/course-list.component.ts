c7257ddb0dc921d298c9e781c164b0a6fb8d181f
andrews-felipe <ferulip@gmail.com>
2019-11-19 16:28:26 -0300

    Ajustando erros de path nos diretórios

--- kahoot/src/app/teacher-module/course/course-list/course-list.component.ts
+++ kahoot/src/app/teacher-module/course/course-list/course-list.component.ts
@@ -14,31 +14,31 @@ export class CourseListComponent implements OnInit, AfterContentChecked {
 
    itens = [
     {
-      image: '../../assets/java.jpg',
+      image: './assets/java.jpg',
       title: 'Orientação objetos com java',
       questions: 3,
       code: "cod. Q2587410",
     },
     {
-      image: '../../assets/nodeJs.png',
+      image: './assets/nodeJs.png',
       title: 'Introdução ao Node.js',
       questions: 8,
       code: "cod. Q2587410"
     },
     {
-      image: '../../assets/vuejs.jpg',
+      image: './assets/vuejs.jpg',
       title: 'Micro front-ends com Vue.js',
       questions: 4,
       code: "cod. Q2587410"
     },
     {
-      image: '../../assets/adonis.jpg',
+      image: './assets/adonis.jpg',
       title: 'Gerando API em node.js com Adonis',
       questions: 4,
       code: "cod. Q2587410"
     },
     {
-      image: '../../assets/kibernetes.jpg',
+      image: './assets/kibernetes.jpg',
       title: 'Introdução a kubernetes',
       questions: 4,
       code: "cod. Q2587410"
